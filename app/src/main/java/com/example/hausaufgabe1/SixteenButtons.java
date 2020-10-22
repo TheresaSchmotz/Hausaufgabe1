6181faf77ccab3053556cdd1615eff95fdac08df
SchmotzTheresaFH <181065355@stud.fh-kufstein.ac.at>
2020-10-22 11:23:34 +0200

    aufgabe 1 fertig

--- app/src/main/java/com/example/hausaufgabe1/SixteenButtons.java
+++ app/src/main/java/com/example/hausaufgabe1/SixteenButtons.java
@@ -3,33 +3,27 @@ package com.example.hausaufgabe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
+
 
 public class SixteenButtons extends AppCompatActivity {
 
+    public int btnAmount = 16;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // Find the ScrollView
-        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
-
-// Create a LinearLayout element
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
 // Add Buttons
-        Button button = new Button(this);
-        button.setText("BUTTON 1");
-        linearLayout.addView(button);
-
-// Add the LinearLayout element to the ScrollView
-        scrollView.addView(linearLayout);
+        LinearLayout mainView = (LinearLayout) findViewById(R.id.sixteenLayout);
+        for (int i = 0; i< btnAmount; i++){
+            Button button = new Button (this);
+            button.setText("BUTTON " + (i+1));
+            button.setId(i);
+            //button.setOnClickListener((View.OnClickListener) this);
+            mainView.addView(button);
+        }
     }
-
 }
-
